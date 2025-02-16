438eaeced12fbc20ef9c27e78cc2cee0501d9bd3
jeevan-budha <jeevan.7budha@gmail.com>
2025-02-16 10:33:21 +0545

    Movie search app API project

---
+++ app.js
@@ -0,0 +1,81 @@
+const searchFrom =document.querySelector("form");
+const movieContainer =document.querySelector(".movie-container");
+const moviePoster =document.querySelector(".movie-poster");
+const movieDetails = document.querySelector(".movie-details");
+const inputBox = document.querySelector(".inputBox");
+
+
+
+async function getmovieInfo(movie){
+    try{
+        const myApiKey ='79629e54';
+        const url =`http://www.omdbapi.com/?apikey=${myApiKey}&t=${movie}`;
+    
+        const response =await fetch(url);
+        if(!response.ok){
+            throw new Error("unable to fetch movie data.");
+        }
+        const data =await response.json();
+        showMovieData(data);
+
+    }catch(error){
+        showErrorMessage("No Movie Found !!!!");
+    }
+   
+}
+
+const showMovieData =(data)=>{
+    movieContainer.innerHTML ="";
+    movieContainer.classList.remove('noBackground');
+    const{Title, imdbRating, Genre, Released, Runtime, Actors, Plot, Poster}=data;
+    const movieElement =document.createElement('div');
+    movieElement.classList.add("movie-info");
+    movieElement.innerHTML=`<h2>${Title}</h2> 
+                            <p><strong>Rating:&#11088;</strong> ${imdbRating}</p>`;
+    const movieGenreElement = document.createElement('div');
+    movieGenreElement.classList.add("movie-genre");
+
+    Genre.split(',').forEach(element =>{
+        const p = document.createElement('p');
+        p.innerText =element;
+        movieGenreElement.appendChild(p);
+    })
+    movieElement.appendChild(movieGenreElement);
+    movieElement.innerHTML +=`<p><strong>Released Date: </strong> ${Released}</p>
+                              <p><strong>Duration: </strong>${Runtime}</p>
+                              <p><strong>Cast: </strong>${Actors}</p>
+                              <p><strong>plot: </strong>${Plot}</p>`;
+    
+    const moviePoster = document.createElement('div');
+    moviePoster.classList.add("movie-poster");
+    moviePoster.innerHTML = `<img src = "${Poster}"/>`
+    movieContainer.appendChild(moviePoster);
+    movieContainer.appendChild(movieElement);
+    
+
+}
+
+
+function showErrorMessage (message){
+    movieContainer.innerHTML=`<h2>${message}</h2>`
+    movieContainer.classList.add('noBackground');
+}
+function handleFormSubmission(e){
+    e.preventDefault();
+    const movieName =inputBox.value.trim();
+    if(movieName !==""){
+        const loader =document.createElement('div');
+        loader.classList.add('loaderdiv');
+        movieContainer.appendChild(loader)
+        // showErrorMessage("Fetching Movie Information .....")
+        getmovieInfo(movieName);
+    }
+    else{
+        showErrorMessage();
+    }
+}
+
+
+searchFrom.addEventListener('submit',handleFormSubmission);
+
+
